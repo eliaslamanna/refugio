c15f02a959c8356c85b24248e40ea33d34ed47ad
matiasbiagetti <biagettimati@gmail.com>
2023-06-10 03:51:24 -0300

    1er commit matias

--- src/main/java/src/Model/Seguimiento.java
+++ src/main/java/src/Model/Seguimiento.java
@@ -1,7 +1,12 @@
 package src.Model;
 
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class Seguimiento {
@@ -22,13 +27,45 @@ public class Seguimiento {
 
     private Adopcion adopcion;
 
+    private VisitaADomicilio getVisitaMasReciente() {
+        VisitaADomicilio visitaMasReciente = this.visitasADomicilio.get(0);
+        for (int i = 1; i<  this.visitasADomicilio.size(); i++){
+            VisitaADomicilio currentVisita = this.visitasADomicilio.get(i);
+            Date currentDate  = currentVisita.getFechaVisita();
+
+            if (currentDate.after(visitaMasReciente.getFechaVisita())){
+                visitaMasReciente = currentVisita;
+            }
+        }
+        return visitaMasReciente;
+    }
     public void enviarRecordatorio(DatosNotificacion datos) {
-        medioNotificacion.enviarNotificacion(datos);
+        VisitaADomicilio visita = getVisitaMasReciente();
+        LocalDate fechaActual = LocalDate.now();
+        Date fechaVisita = visita.getFechaVisita();
+        long diferenciaEnDias = ChronoUnit.DAYS.between(fechaActual, fechaVisita.toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+        if (diferenciaEnDias <= this.diasRecordatorio){
+            medioNotificacion.enviarNotificacion(datos);
+        }
     }
 
-    // TODO mover diasRecordatorio
-    public void cambiarMetodoNotificacion(EstrategiaNotificacion metodoPreferido, int diasRecordatorio) {
+    public void elegirMetodoNotificacion(EstrategiaNotificacion metodoPreferido) {
         this.medioNotificacion = metodoPreferido;
+    }
+
+    public void crearVisita(VisitaDTO visitaDTO) {
+        VisitaADomicilio newVisita = new VisitaADomicilio( getProximaFechaDeVisita(getVisitaMasReciente().getFechaVisita()), visitaDTO.getObservaciones(), visitaDTO.getEncuesta());
+        this.visitasADomicilio.add(newVisita);
+    }
+
+    private Date getProximaFechaDeVisita(Date ultimaVisita){
+        Calendar calendario = Calendar.getInstance();
+
+        calendario.setTime(ultimaVisita);
+
+        calendario.add(Calendar.DAY_OF_YEAR, this.cadenciaVisita);
+
+        return calendario.getTime();
     }
 
 }
